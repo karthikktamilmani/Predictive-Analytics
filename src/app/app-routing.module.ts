a8bb52057bd819f7f4b90c54d3a1b47c56cef017
Sharmila Thirumalainathan <sh946344@dal.ca>
2019-11-23 23:14:29 -0400

    Added layout route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VizComponent } from './viz/viz.component';
-
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  { component:VizComponent , path: 'viz'}
+  { component: LayoutComponent, path: '' },
+  { component: VizComponent, path: 'viz' }
 ];
 
 @NgModule({
